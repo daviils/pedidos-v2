a9e74c9e25cecbb4efa301ecef40d9cf47e311ec
david <davidsilvaluis@hotmail.com>
2026-07-01 00:22:09 -0300

    adjust env

--- apps/admin/src/environments/environment.ts
+++ apps/admin/src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: false,
-  baseUrl: 'http://localhost:3000',
-  productImageBaseUrl:
-    'https://pedidos2.blob.core.windows.net/product-images/products/',
+  baseUrl: 'https://pedidos-api-c3fzgra2hvf3aahp.canadacentral-01.azurewebsites.net',
+  productImageBaseUrl:'https://pedidos2.blob.core.windows.net/product-images/products/',
 };
